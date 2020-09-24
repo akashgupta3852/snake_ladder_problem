e7578a3f7b7515ce3ce3105909c9297ea4f090c0
akashgupta3852 <akashgupta3852@gmail.com>
2020-09-24 19:42:29 +0530

    [Akash]Added: dice number

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,5 +4,9 @@ public class SnakeLadder{
 
 		//constants
 		int INITIAL_POSITION=0, NO_OF_PLAYERS=1;
+
+		//computation
+		int face_number=(int)Math.floor(Math.random()*10)%6 + 1;
+		System.out.println(face_number);
 	}
 }
