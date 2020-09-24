3fef64eef251fb834ce8093373d1872324a6795f
akashgupta3852 <akashgupta3852@gmail.com>
2020-09-24 20:34:00 +0530

    [Akash]Refactored: position after every die roll

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -5,10 +5,12 @@ public class SnakeLadder{
 		//constants
 		int INITIAL_POSITION=0, NO_OF_PLAYERS=1;
 		int NO_PLAY=0,LADDER=1,SNAKE=2;
+		int count=0;
 
 		//computation
 		while(INITIAL_POSITION<100)
 		{
+			count++;
 			int face_number=(int)Math.floor(Math.random()*10)%6 + 1;
 			int check=(int)Math.floor(Math.random()*10)%3;
 			switch(check)
@@ -24,7 +26,8 @@ public class SnakeLadder{
 					INITIAL_POSITION-=face_number;
 					break;
 			}
+			System.out.println(INITIAL_POSITION);
 		}
-		System.out.println(INITIAL_POSITION);
+		System.out.println(count);
 	}
 }
