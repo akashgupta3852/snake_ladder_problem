aee75757a744bd328cd1564ac9b760d70de0d68d
akashgupta3852 <akashgupta3852@gmail.com>
2020-09-24 19:29:16 +0530

    [Akash]Refactored: initial position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,5 +1,8 @@
 public class SnakeLadder{
 	public static void main(String[] args){
 		System.out.println("Welcome to Snake Ladder Problem Statement");
+
+		//constants
+		int INITIAL_POSITION=0, NO_OF_PLAYERS=1;
 	}
 }
